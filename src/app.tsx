b4fdaeecc5c73210b6c768ea111b03a1efd96c78
Tavi Castro <hanminhdanh1325@gmail.com>
2025-11-21 13:48:43 +0700

    feat: add boot splash screen

--- src/app.tsx
+++ src/app.tsx
@@ -5,11 +5,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DemoScreen from '@/screens/DemoScreen';
 import { ThemeProvider } from '@/theme/context';
+import BootSplash from 'react-native-bootsplash';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
 
   return (
     <SafeAreaProvider>
